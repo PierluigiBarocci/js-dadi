6bc2d4f21382fd8ad56a85987203fe18e1ed70dc
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-15 16:47:29 +0100

    Creazione Files

---
+++ main.js
@@ -0,0 +1,4 @@
+// Esercitazione 2 => repo: js-dadi
+// Generare un numero casuale tra 1 e 6.
+// Attraverso un prompt chiedere all'utente un numero tra 1 e 6.
+// Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).
