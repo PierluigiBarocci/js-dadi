ca3f90ca2486916d6f1b7fecaf9b9ace0325303d
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-15 17:03:55 +0100

    Test con un array per capire se veramente mi sta pescando numeri da 1 a 6, la risposta è sì

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 // Generare un numero casuale tra 1 e 6.
 // Attraverso un prompt chiedere all'utente un numero tra 1 e 6.
 // Stampare un messaggio per dire all'utente se ha vinto oppure no (il numero più alto vince).
+
+// prova per vedere se effettivamente la funzione sta pescando numeri a caso tra 1 e 6
+var insieme = [];
+for (var i = 0; i < 50 ; i++) {
+    var num = Math.floor(Math.random() * 6) + 1;
+    insieme.push(num);
+}
+console.log(insieme);
